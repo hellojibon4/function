623570f4435b248235adc986c5c4ebf946f23c2a
hellojibon4 <hellojibon4@gmail.com>
2025-08-16 19:37:09 +0600

    add a new code

---
+++ peramitar.js
@@ -0,0 +1,21 @@
+function displayDate() {
+    console.log("Date function called");
+    console.log("Date function called");
+    console.log("Date function called");
+    console.log("Date function called");
+    console.log("Date function called");
+    console.log("Date function called");
+    console.log("Date function called");
+    console.log("Date function called");
+}
+// displayDate();
+
+function square(num) {
+    console.log(num);
+   const result = num * num;
+   console.log("Squrare of the given number is : ", result);
+}
+
+square(5,);
+console.log('------------------------');
+square(54);
