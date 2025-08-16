749ece0096ee824ac1db52bf255287f46b2d2fbc
hellojibon4 <hellojibon4@gmail.com>
2025-08-16 22:58:23 +0600

    vesiyas-perams.js

---
+++ recap.js
@@ -0,0 +1,28 @@
+function add (num1, num2){
+    const total = num1 + num2;
+    return total;
+}
+
+const bill = add ( 5, 85);
+console.log("The total bill is:", bill);
+
+
+function add (num1, num2){
+  return num1 + num2;
+}
+
+const bill2 = add(10, 20);
+console.log("The total bill is:", bill2);
+
+
+function doMath( num1, num2){
+    const sum  = num1 + num2;
+    const diff = num1 - num2;
+    const multiplay = sum * diff;
+    const result = multiplay /2;
+    return result;
+}
+
+const result = doMath(132, 43);
+console.log(result);
+
