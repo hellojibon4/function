e21b1c75504f0b1d111fe26b2ac464863a928874
hellojibon4 <hellojibon4@gmail.com>
2025-08-16 20:18:33 +0600

    mf

---
+++ argumant.js
@@ -0,0 +1,11 @@
+function doubleIt (number){
+    const doubled = number * 2;
+    console.log("The double of the given number is : ", number, doubled);
+}
+
+console.log("Calling doubleIt function with 235");
+doubleIt(235);
+console.log("Calling doubleIt function with 1470");
+doubleIt(1470);
+console.log("Calling doubleIt function with 1455");
+doubleIt(1455);
