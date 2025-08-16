c0a78a0ecca027a1eca24975bf9ceebcaad705d8
hellojibon4 <hellojibon4@gmail.com>
2025-08-16 19:46:46 +0600

    add  to 2nd code

--- peramitar.js
+++ peramitar.js
@@ -11,11 +11,22 @@ function displayDate() {
 // displayDate();
 
 function square(num) {
-    console.log(num);
+    console.log('The given number is : ', num);
    const result = num * num;
    console.log("Squrare of the given number is : ", result);
 }
 
-square(5,);
-console.log('------------------------');
-square(54);
+// square(5,);
+// console.log('------------------------');
+// square(54);
+
+
+function add (num1,num2){
+    const sum = num1 + num2;
+    console.log("sum of the given numbers is : ", sum);
+
+}
+add( 23, 98);
+add( 12, 34);
+add( 45, 67);
+add( 78, 90);
